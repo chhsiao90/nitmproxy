2f57df623673e90ac5cc8a91a6d2c64691c1967a
Karsten Ohme <kohme@gigsky.com>
2021-10-18 06:12:52 +0200

    logic for handling backend channel after connection

--- src/main/java/com/github/chhsiao90/nitmproxy/channel/BackendChannelBootstrap.java
+++ src/main/java/com/github/chhsiao90/nitmproxy/channel/BackendChannelBootstrap.java
@@ -4,20 +4,29 @@ import com.github.chhsiao90.nitmproxy.ConnectionContext;
 import com.github.chhsiao90.nitmproxy.NitmProxyMaster;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.*;
 
 public class BackendChannelBootstrap {
     public ChannelFuture connect(ChannelHandlerContext fromCtx,
                                  NitmProxyMaster master,
                                  ConnectionContext connectionContext,
                                  ChannelHandler handler) {
-        return new Bootstrap()
+        ChannelFuture channelFuture = new Bootstrap()
                 .group(fromCtx.channel().eventLoop())
                 .channel(fromCtx.channel().getClass())
                 .handler(handler)
                 .connect(connectionContext.getServerAddr().getHost(),
                          connectionContext.getServerAddr().getPort());
+        if (master.config().getOnConnectHandler() != null) {
+            channelFuture.addListener((ChannelFutureListener) channelFuture1 -> {
+                if (channelFuture1.isSuccess()) {
+                    Channel channel = channelFuture1.channel();
+                    master.config().getOnConnectHandler().accept(channel);
+                }
+            });
+        }
+        return channelFuture;
     }
+
+
 }
